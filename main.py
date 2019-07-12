8845ee8df5786601a81344845b01217f8344d852
Jonas Pickholtz <jpickhol@gmail.com>
2019-07-12 13:50:48 +0300

    added show path

--- main.py
+++ main.py
@@ -30,11 +30,14 @@ def browse():
 
 @route('/ajax/show/<showname>')
 def display_show(showname):
+    sectionData = utils.displayShow(showname)
+    return template("./templates/show.tpl", result=sectionData)
+
+@route('/show/<showname>')
+def display_show(showname):
     sectionTemplate = "./templates/show.tpl"
     sectionData = utils.displayShow(showname)
     return template("./pages/index.html", version=utils.getVersion(), sectionTemplate=sectionTemplate, sectionData=sectionData)
-
-
 
 @route('/search')
 def search():
